17080c5af149dc7dcc974f74f8e00ca7acc1ee40
ephraimkariuki <ephraimkabuikariuki@gmail.com>
2025-04-11 16:18:12 +0300

    use of user_signed_in? and current_user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def new
